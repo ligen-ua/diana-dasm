54ebcad094dce34b17730679271c8545f86a816b
ligen-ua <ligen.work@gmail.com>
2023-05-22 13:11:45 +0300

    sf

--- src/orthia/orthia_disasm_ui/ui_disasm_window.cpp
+++ src/orthia/orthia_disasm_ui/ui_disasm_window.cpp
@@ -1,7 +1,24 @@
 #include "ui_disasm_window.h"
 
-CDisasmWindow::CDisasmWindow(std::function<oui::String()> getCaption)
+CDisasmWindow::CDisasmWindow(std::function<oui::String()> getCaption,
+    std::shared_ptr<orthia::CProgramModel> model)
     :
+    m_model(model),
     oui::SimpleBrush<oui::CPanelWindow>(getCaption)
 {
 }
+void CDisasmWindow::SetActiveItem(int itemUid)
+{
+    m_itemUid = itemUid;
+    m_offset = 0;
+    Invalidate();
+}
+void CDisasmWindow::DoPaint(const oui::Rect& rect, oui::DrawParameters& parameters)
+{
+    Parent_type::DoPaint(rect, parameters);
+    if (m_itemUid)
+    {
+        // m_model->Get
+    }
+}
+
