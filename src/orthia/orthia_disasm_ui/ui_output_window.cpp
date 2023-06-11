ea2efadd5219feaabb7bb3693b9d102345591000
ligen-ua <ligen.work@gmail.com>
2023-06-11 15:14:30 +0300

    Initial design of multilineview

--- src/orthia/orthia_disasm_ui/ui_output_window.cpp
+++ src/orthia/orthia_disasm_ui/ui_output_window.cpp
@@ -6,8 +6,29 @@ COutputWindow::COutputWindow(std::function<oui::String()> getCaption)
 {
     // FOR WIN LOGIC DEBUG
     //    SetBackgroundColor(oui::ColorBlue());
+
+    m_colorProfile = std::make_shared<oui::DialogColorProfile>();
+    QueryDefaultColorProfile(*m_colorProfile);
+
+    oui::IMultiLineViewOwner* param = this;
+    m_view = std::make_shared<oui::CMultiLineView>(m_colorProfile, param);
+
 }
 void COutputWindow::AddLine(const oui::String& line)
 {
-    m_lines.push_back(line);
+    oui::MultiLineViewItem item;
+    item.text = line;
+    m_view->AddLine(std::move(item));
+}
+void COutputWindow::CancelAllQueries()
+{
+}
+void COutputWindow::ConstructChilds()
+{
+    AddChild(m_view);
+}
+void COutputWindow::OnResize()
+{
+    const oui::Rect clientRect = GetClientRect();
+    m_view->Resize(clientRect.size);
 }
