c5164e57a0530427e82ff8b42e43343ab8be6046
ligen-ua <ligen.work@gmail.com>
2023-06-18 17:17:09 +0300

    Added bad memory support feature

--- src/orthia/orthia_disasm_ui/ui_disasm_window.cpp
+++ src/orthia/orthia_disasm_ui/ui_disasm_window.cpp
@@ -52,42 +52,21 @@ void CDisasmWindow::ReloadVisibleData()
     const int maxStepForwardBytes = 1024;
     const int maxStepBackwardBytes = m_userSuppliedPeAddress ? 0: 256 ;
 
-    if (m_peAddress < rangeInfo.address)
-    {
-        m_peAddress = rangeInfo.address;
-    }
-    else
-    if (m_peAddress > rangeInfo.lastValidAddress)
-    {
-        m_view->Clear();
-        return;
-    }
-    
     auto routeStart = item->GetModuleManager()->QueryRouteStart(m_peAddress);
     if (!routeStart || (m_peAddress - routeStart) > (orthia::Address_type)maxStepBackwardBytes)
     {
         // no route or it it too far away (which is strange, whatever)
         routeStart = m_peAddress - maxStepBackwardBytes;
-        if (routeStart < rangeInfo.address)
+    }
+
+    {
+        // check if route start is in another range
+        auto routeInfo = item->GetRangeInfo(routeStart);
+        if (routeInfo.address != rangeInfo.address)
         {
             routeStart = rangeInfo.address;
         }
     }
-
-    // final check bounds
-    auto routeOffset = routeStart - rangeInfo.address;
-    if (routeOffset > rangeInfo.size)
-    {
-        m_view->Clear();
-        return;
-    }
-    if (m_peAddress < routeStart)
-    {
-        // something is terribly wrong
-        m_view->Clear();
-        return;
-    }
-
     struct DisasmWriter:orthia::ITextPrinter
     {
         std::vector<oui::MultiLineViewItem> items;
@@ -108,6 +87,8 @@ void CDisasmWindow::ReloadVisibleData()
         DisasmWriter& m_writer;
         bool m_firstPrint = true;
         orthia::Address_type m_firstVirtualOffset;
+        const char* m_pDataFlags = 0;
+        orthia::Address_type m_routeStart = 0;
     public:
         MemoryPrinter(orthia::ITextPrinter* pTextPrinter,
             int dianaMode,
@@ -124,6 +105,22 @@ void CDisasmWindow::ReloadVisibleData()
         {
             m_bytesIdent += 10;
             m_countOfSpacesAfterAddress = 3;
+            m_printInvalidPages = true;
+        }
+        void SetFlags(const char* pDataFlags, orthia::Address_type routeStart)
+        {
+            m_pDataFlags = pDataFlags;
+            m_routeStart = routeStart;
+        }
+        bool IsBadByte(orthia::Address_type virtualOffset) override
+        {
+            if (m_pDataFlags)
+            {
+                auto relativeOffset = virtualOffset - m_routeStart;
+                auto & flag = m_pDataFlags[relativeOffset];
+                return flag & orthia::WorkAddressData::dataFlags_Invalid;
+            }
+            return false;
         }
         void Preprocess(int iRes,
             ::DianaContext& context,
@@ -167,10 +164,17 @@ void CDisasmWindow::ReloadVisibleData()
         m_view->Clear();
         return;
     }
+
     orthia::VmMemoryRangeInfo vmRangeInfo;
     vmRangeInfo.address = routeStart;
     vmRangeInfo.size = maxSizeToUse;
     vmRangeInfo.flags = vmRangeInfo.flags_hasData;
+
+    if (data.rangeFlags & orthia::WorkAddressData::flags_FullInvalid)
+    {
+        vmRangeInfo.flags = 0;
+    }
+    printer.SetFlags(data.pDataFlags, routeStart);
     printer.OnRange(vmRangeInfo, data.pDataStart);
 
     m_view->Init(std::move(writer.items));
@@ -199,6 +203,7 @@ bool CDisasmWindow::ScrollUp(oui::MultiLineViewItem* item, int count)
             m_view->SetCursorYPos(0);
             return true;
         }
+        --count;
     }
     if (m_peAddress > count)
     {
