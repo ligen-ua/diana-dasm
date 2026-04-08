27226504ba8676b43654f80ca8edaefe5cb56611
Ray Evko <rayevko@gmail.com>
2026-04-08 13:32:43 +0300

    CMake improvements

--- src/orthia/orthia_disasm_ui/orthia_model_interfaces.h
+++ src/orthia/orthia_disasm_ui/orthia_model_interfaces.h
@@ -54,6 +54,8 @@ namespace orthia
         std::function<void(WorkAddressData*)> completeHandler;
 
         WorkAddressData()
+            :
+                rangeFlags(flags_FullInvalid)
         {
         }
         ~WorkAddressData()
